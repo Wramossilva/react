7dc95664ece1172ec212ca708d493cd4b6cb026b
RAMOS-SILVA <wilfried.ramossilva@gmail.com>
2019-04-30 10:48:49 +0200

    form_fruit

--- src/components/fruits/FruitApp.js
+++ src/components/fruits/FruitApp.js
@@ -1,47 +1,43 @@
 import React from 'react';
 import FruitList from './FruitList';
-import FruitForm from './FruitForm'
+import FruitForm from './FruitForm';
+
 class FruitApp extends React.Component {
 
-    constructor() {
-        super();
+   constructor(){
+      super();
 
-        this.state = {
-            fruitData: ["Pommes", "Poires", "Tomates"]
+      this.state = {
+         fruitData: ["Pommes", "Poires", "Tomates"]
+      }
 
-        }
-        this.deleteFruit = this.deleteFruit.bind(this);
-        this.addFruit = this.addFruit.bind(this);
-    }
+      this.deleteFruit = this.deleteFruit.bind(this);
+      this.addFruit = this.addFruit.bind(this);
+   }
 
-    deleteFruit(index) {
-        let fruits = this.state.fruitData;
-        fruits.splice(index, 1);
-        this.setState({ fruitData: fruits });
+   deleteFruit(index){
+      let fruits = this.state.fruitData;
+      fruits.splice(index,1);
+      this.setState({fruitData: fruits});
+   }
 
-    }
+   addFruit(fruitName){
+      let fruits = this.state.fruitData;
+      fruits.push(fruitName);
+      fruits.sort();
+      this.setState({fruitData: fruits});
+   }
 
-    addFruit(fruitName){
-        let fruits = this.state.fruitData;
-        fruits.push(fruitName);
-        this.setState({fruitData: fruits});
-    }
-
-    render() {
-        return (<div>
-            <h1>Listes des fruits </h1>
-            <FruitForm/>
-            <FruitList
-                list={this.state.fruitData}
-                removeItem={this.deleteFruit}
-            />
-
-
-
-
-        </div>);
-    }
+   render(){
+      return (<div>
+         <h1>Listes des fruits </h1>
+         <FruitForm addItem={this.addFruit}/>
+         <FruitList 
+            list={this.state.fruitData} 
+            removeItem={this.deleteFruit}
+         />
+      </div>);
+   }
 }
-
 
 export default FruitApp;
--- src/components/fruits/FruitForm.js
+++ src/components/fruits/FruitForm.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
-function FruitForm(props){
-    return(
-        <form>
-            <input type="text" placeholder="Votre fruit"/>
-            <button type="submit">Ajouter</button>
-        </form>
-    )
+class FruitForm extends React.Component{
+
+   constructor(props){
+      super(props);
+      this.onSubmit = this.onSubmit.bind(this);
+   }
+
+   onSubmit(event){
+      event.preventDefault();
+      let newFruit = this.refs.fruitInput.value.trim();
+
+      if(newFruit){
+         this.props.addItem(newFruit);
+         this.refs.fruitInput.value = '';
+      }
+   }
+
+   render(){
+      return (
+         <form onSubmit={this.onSubmit}>
+             <input type="text" 
+                ref="fruitInput"
+                placeholder="Votre fruit" />
+             <button type="submit">Ajouter</button>
+          </form> 
+       );
+   }
+   
 }
+
 export default FruitForm;
